ba16b1a356ec0fda7cbe48739544a1dab9eb65dd
phemy <phemy.smith@gmail.com>
2017-10-23 14:47:35 +0100

    adding test casesto the test

---
+++ tests/test.js
@@ -0,0 +1,38 @@
+const assert = require('chai').assert;
+const myApp = require('../src/primeFactor.js');
+
+	describe("primeFactor", () => {
+		describe("handle valid input", ()=> {
+			it("should display pling as 3 is a prime factor ", () => {
+				assert.equal(myApp.factor(3), "pling");
+
+			});
+		})
+
+		describe("handle valid input", ()=> {
+			it("should display plang as 5 is a prime factor ", () => {
+				assert.equal(myApp.factor(5), "plang");
+
+			});
+		})
+		describe("handle valid input", ()=> {
+			it("should display plong as 3 is a prime factor ", () => {
+				assert.equal(myApp.factor(7), "plong");
+
+			});
+		})
+
+		describe("handle valid input", ()=> {
+			it("should display '34' as not knowing what to do ", () => {
+				assert.equal(myApp.factor(34), 34);
+
+			});
+		})
+		describe("handle valid input", ()=> {
+			it("should display '50' as not knowing what to do ", () => {
+				assert.equal(myApp.factor(50), 50);
+
+			});
+		})
+
+	})
